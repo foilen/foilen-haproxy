9a39d06f162ab16d2547b08e75dd281025071512
Simon Levesque <simon@foilen.com>
2021-08-09 08:11:43 -0400

    Update haproxy to v1.7.14

--- docker/compile.sh
+++ docker/compile.sh
@@ -5,7 +5,10 @@ set -e
 echo ++++[ OpenSSL - Git clone ]++++
 cd /build
 if [ -d openssl ]; then
-    echo Already cloned
+    echo Already cloned. Fetching
+    pushd openssl
+    git fetch
+    popd
 else
     git clone git://git.openssl.org/openssl.git openssl
 fi
@@ -22,14 +25,17 @@ make && make install_sw
 echo ++++[ HA Proxy - Git clone ]++++
 cd /build
 if [ -d haproxy ]; then
-    echo Already cloned
+    echo Already cloned. Fetching
+    pushd haproxy
+    git fetch
+    popd
 else
     git clone http://git.haproxy.org/git/haproxy-1.7.git/ haproxy
 fi
 
 echo ++++[ HA Proxy - Git switch branch ]++++
 cd /build/haproxy
-git checkout v1.7.12
+git checkout v1.7.14
 
 echo ++++[ HA Proxy - Compile ]++++
 make \
